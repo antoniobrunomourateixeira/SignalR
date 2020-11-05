ea613a392de30532f18bb6debf968fa23a2570aa
antoniobrunomourateixeira <antoniobrunobm@gmail.com>
2020-11-05 11:18:15 -0300

    files .net core

---
+++ ChatSignalR/Controllers/Chat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatSignalR.Interfaces;
+using ChatSignalR.Model;
+using ChatSignalR.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSignalR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Chat : Controller
+    {
+        private IHubContext<ChatHub, IChatHub> _hubContext;
+
+        public Chat(IHubContext<ChatHub, IChatHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public string Post([FromBody] Message msg)
+        {
+            string retMessage = string.Empty;
+            try
+            {
+                var x= _hubContext.Clients.All.SendNotification(msg.type, msg.message);
+                retMessage = "Success";
+            }
+            catch (Exception e)
+            {
+                retMessage = e.ToString();
+            }
+            return retMessage;
+        }
+    }
+}
---
+++ ChatSignalR/Interfaces/IChatHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatSignalR.Interfaces
+{
+    public interface IChatHub
+    {
+        Task SendNotification(string type, string message);
+    }
+}
